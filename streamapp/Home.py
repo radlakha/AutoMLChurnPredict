bd67aca5caa41d08d968e9f011b03e81fe762b10
Shreya Bhadwal <shreya.bhadwal@gmail.com>
2023-01-17 23:13:07 +0530

    Streamlit App 1

---
+++ streamapp/Home.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 13 11:55:38 2023
+
+@author: sbhadwal
+"""
+
+import streamlit as st
+
+
+st.title("Churn Prediction")
+st.write("Traverse the index in order")
