1b69726ae6b5b0094f2563bc226a2cac7d3510dc
Shreya Bhadwal <shreya.bhadwal@gmail.com>
2023-01-25 12:37:43 +0530

    Updates to the data statistics display file

--- 3_Know_Your_Data.py
+++ 3_Know_Your_Data.py
@@ -10,21 +10,23 @@ from pycaret.classification import *
 import os 
 import pandas as pd
 
-path = 'C:/Users/sbhadwal/sol1/streamapp/data_mod'
+path = 'C:/Users/sbhadwal/sol1/streamapp/data_mod' #Defining paths
 dir_list = os.listdir(path)
-st.title("Visualiziation and Simple Statistics")
 path_final = path + "/" + dir_list[0]
-data = pd.read_csv(path_final) 
-cols = list(data.columns)
+
+st.title("Visualiziation and Simple Statistics") #Title
+data = pd.read_csv(path_final) #Read the data into a dataframe
+cols = list(data.columns) #Columns list
 
 st.write("Simple Statistics on Your Data")
-data_stat = data.describe().apply(lambda s: s.apply(lambda x: format(x, 'f')))
-st.write(data_stat)
-target = st.session_state.target
+data_stat = data.describe().apply(lambda s: s.apply(lambda x: format(x, 'f'))) #Describe data
+st.write(data_stat) #Display obtained dataframe on screen
+
+#Ignore
+
+#target = st.session_state.target
 # st.write(st.session_state) Debugging
 
 #count = st.selectbox("Distribution of", cols, key='count')
 
-cols2 = list(data_stat.columns)
-choice = st.selectbox("Count Distribution of", cols2)
 
