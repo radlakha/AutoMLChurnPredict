968c52a0421af54c6c5987ff51db3d60d3688fb2
Shreya Bhadwal <shreya.bhadwal@gmail.com>
2023-02-14 12:53:43 +0530

    Code updated to ensure no custom paths needed

--- pages/4_Train_Your_Model.py
+++ pages/4_Train_Your_Model.py
@@ -9,16 +9,19 @@ import os
 import pandas as pd
 import streamlit as st
 from pycaret.classification import *
+import pathlib
 
-path = 'C:/Users/sbhadwal/sol1/streamapp/data_mod' #Define path
+parent_path = pathlib.Path(__file__).parent.parent.resolve() 
+path = os.path.join(parent_path, "data_mod")
 dir_list = os.listdir(path)
-path_final = path + "/" + dir_list[0]
+path_final = path + "/" + dir_list[0]   #Defining the final path
 data = pd.read_csv(path_final) 
 # st.write(st.session_state) 
 target = st.session_state.tkey #Use the target taken from the previous page
 
 st.title("Model Results") #Title
-s = setup(data, target = target, html=False, silent=True) #Data preprocessing function of Pycaret
+s = setup(data, target = target, html=False, silent=True, normalize = True,
+          fix_imbalance=True) #Data preprocessing function of Pycaret
 best_model = compare_models() #Compare diff models
 model_results = pull() #Pull them to display as a dataframe
 st.write(model_results) #Display the table
